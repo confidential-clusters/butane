77395a69fa0c3c26ff3f8b3f8b37a0d2289e4a00
Alice Frosi <afrosi@redhat.com>
2026-08-11 12:08:24 +0000

    base/v0_7: use ignition v3_7_experimental types for attestation support
    Signed-off-by: Alice Frosi <afrosi@redhat.com>

--- base/v0_7/translate_test.go
+++ base/v0_7/translate_test.go
@@ -30,7 +30,7 @@ import (
 	"github.com/coreos/butane/translate"
 
 	"github.com/coreos/ignition/v2/config/util"
-	"github.com/coreos/ignition/v2/config/v3_6/types"
+	"github.com/coreos/ignition/v2/config/v3_7_experimental/types"
 	"github.com/coreos/vcontext/path"
 	"github.com/coreos/vcontext/report"
 	"github.com/stretchr/testify/assert"
